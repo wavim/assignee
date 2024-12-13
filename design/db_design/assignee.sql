929e1594d076dc1a77c1515ae18aa9554601a7c6
carbonsoda <carbonsoda@outlook.com>
2024-12-13 19:35:45 +0800

    prisma introspect

--- design/db_design/assignee.sql
+++ design/db_design/assignee.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 9.1.0, for Win64 (x86_64)
 --
--- Host: localhost    Database: assignee
+-- Host: localhost    Database: assigneedb
 -- ------------------------------------------------------
 -- Server version	9.1.0
 
@@ -310,4 +310,4 @@ CREATE TABLE `users` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-12-09 18:01:48
+-- Dump completed on 2024-12-09 18:43:26
