debc4845139b13143c7ec320736c7f4007f13905
carbonsoda <carbonsoda@outlook.com>
2025-01-31 14:11:16 +0800

    merge.sh push all local branches

--- merge.sh
+++ merge.sh
@@ -9,14 +9,14 @@ for branch in $branches; do
     git merge "$branch"
 done
 
-git push origin master
-
 for branch in $branches; do
     echo -e "\nUpdating $branch with latest master..."
     git switch "$branch"
     git merge master
-    git push origin "$branch"
 done
 
 echo
 git switch master
+
+echo
+git push --all origin
