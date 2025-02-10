aa1a5cb1cc13b98a6d0468c6b9821f1898623fde
carbonsoda <carbonsoda@outlook.com>
2025-02-10 19:25:41 +0800

    minor refactor

--- site/backend/src/prisma/extensions/metadata.extension.ts
+++ site/backend/src/prisma/extensions/metadata.extension.ts
@@ -1,5 +1,4 @@
 import { Prisma } from "@prisma/client";
-import { getTimestamp } from "utils/timestamp.util.js";
 
 const TABLES_WITH_CREATED = [
 	"User",
@@ -15,12 +14,13 @@ const TABLES_WITH_CREATED = [
 	"SubmissionAttachment",
 ];
 const TABLES_WITH_UPDATED = ["User", "Password", "Preference", "Team", "Assignment", "Submission"];
+const TIME_STAMP = () => BigInt(Date.now()) / 1000n;
 
 export const TableMetadataExtension = Prisma.defineExtension({
 	query: {
 		$allModels: {
 			create({ model, args, query }) {
-				const now = getTimestamp();
+				const now = TIME_STAMP();
 				//@ts-ignore
 				if (TABLES_WITH_CREATED.includes(model)) args.data.created = now;
 				//@ts-ignore
@@ -30,7 +30,7 @@ export const TableMetadataExtension = Prisma.defineExtension({
 				return query(args);
 			},
 			createMany({ model, args, query }) {
-				const now = getTimestamp();
+				const now = TIME_STAMP();
 				if (!("length" in args.data)) {
 					//@ts-ignore
 					if (TABLES_WITH_CREATED.includes(model)) args.data.created = now;
@@ -51,14 +51,14 @@ export const TableMetadataExtension = Prisma.defineExtension({
 				return query(args);
 			},
 			update({ model, args, query }) {
-				const now = getTimestamp();
+				const now = TIME_STAMP();
 				//@ts-ignore
 				if (TABLES_WITH_UPDATED.includes(model)) args.data.updated = now;
 				else throw new Error(`Query attempts to update readonly field: ${query}`);
 				return query(args);
 			},
 			updateMany({ model, args, query }) {
-				const now = getTimestamp();
+				const now = TIME_STAMP();
 				//@ts-ignore
 				if (TABLES_WITH_UPDATED.includes(model)) args.data.updated = now;
 				else throw new Error(`Query attempts to update readonly field: ${query}`);
--- site/backend/src/utils/timestamp.util.ts
+++
@@ -1,1 +0,0 @@
-export const getTimestamp = () => BigInt(Date.now()) / 1000n;
