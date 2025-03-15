a0974a9fd40ff7d3bb9212ed36a14e7eb6a31c96
carbonsoda <carbonsoda@outlook.com>
2025-03-15 20:46:28 +0800

    routing changes

--- site/frontend/src/routes.ts
+++ site/frontend/src/routes.ts
@@ -2,12 +2,8 @@ import { RouteDefinition } from "@solidjs/router";
 import { lazy } from "solid-js";
 
 export const routes: RouteDefinition[] = [
-	{
-		path: "/",
-		component: lazy(() => import("./pages/Home/Home")),
-	},
-	{
-		path: "**",
-		component: lazy(() => import("./pages/404/404")),
-	},
+	//MO TODO "/" shall route to actual app, and redirect to "home" if unauthorized
+	{ path: "/", component: lazy(() => import("./pages/Home/Home")) },
+	{ path: "/home", component: lazy(() => import("./pages/Home/Home")) },
+	{ path: "**", component: lazy(() => import("./pages/404/404")) },
 ];
