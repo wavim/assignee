05769578b80c0788672a8fd4d85cffd750ef519e
carbonsoda <carbonsoda@outlook.com>
2025-05-06 22:16:25 +0800

    variants for mobile button feedback

--- site/frontend/src/components/Button/Button.tsx
+++ site/frontend/src/components/Button/Button.tsx
@@ -13,8 +13,8 @@ export default (props: {
 			(props.class ?? "")
 		}
 	>
-		<div class="bg-button-invert absolute top-0 right-0 left-0 h-full origin-right scale-x-0 transition-transform duration-300 ease-in-out group-hover:origin-left group-hover:scale-x-100"></div>
-		<span class="text-button-invert font-jakarta group-hover:text-button z-10 p-3 text-xl transition-colors duration-300 ease-in-out">
+		<div class="bg-button-invert absolute top-0 right-0 left-0 h-full origin-right scale-x-0 transition-transform duration-300 ease-in-out group-hover:origin-left group-hover:scale-x-100 group-active:scale-x-100 group-active:transition-none"></div>
+		<span class="text-button-invert font-jakarta group-hover:text-button group-active:text-button z-10 p-3 text-xl transition-colors duration-300 ease-in-out group-active:transition-none">
 			<span class="inline-block pr-1 transition-transform duration-300 ease-in-out group-hover:translate-x-1">
 				{props.children}
 			</span>
