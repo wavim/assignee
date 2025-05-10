0e662c773ae042c7b7380cbdfa65c3bdf80a162d
carbonsoda <carbonsoda@outlook.com>
2025-05-10 23:52:56 +0800

    F: initial i18n setup

--- site/frontend/src/pages/Home/Hero/Hero.tsx
+++ site/frontend/src/pages/Home/Hero/Hero.tsx
@@ -1,15 +1,17 @@
+import { useI18n } from "../Context";
 import Login from "./Login/Login";
 
 export default () => {
+	const [t] = useI18n();
+
 	return (
 		<section class="flex justify-around">
 			<div class="flex w-full flex-col gap-7 px-7 pt-24">
 				<h1 class="font-jakarta text-text-primary w-4/5 text-4xl">
-					Your simplest app for assignments
+					{t("hero.header")}
 				</h1>
 				<p class="font-jakarta text-text-secondary w-full text-xl">
-					Assign, return and grade with utmost ease – from business to school to
-					hobbies and more
+					{t("hero.subtitle")}
 				</p>
 				<Login></Login>
 			</div>
--- site/frontend/src/pages/Home/Home.tsx
+++ site/frontend/src/pages/Home/Home.tsx
@@ -1,14 +1,15 @@
 import Header from "../../components/Header/Header";
+import Context, { useI18n } from "./Context";
 import Hero from "./Hero/Hero";
 
 export default () => {
 	//MO DEV height for scroll
 	return (
-		<>
+		<Context>
 			<Header></Header>
 			<main class="flex h-[1000vh] w-full flex-col">
 				<Hero></Hero>
 			</main>
-		</>
+		</Context>
 	);
 };
