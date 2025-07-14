f7572161e934817753d99bafa66283e4854672f1
carbonsoda <carbonsoda@outlook.com>
2025-07-14 13:27:54 +0800

    F: rework init script

--- site/frontend/src/index.tsx
+++ site/frontend/src/index.tsx
@@ -1,15 +1,32 @@
-import "./init";
 import "./styles/index.css";
+
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/all";
+import { natlog } from "natural-log";
+
+gsap.registerPlugin(ScrollTrigger);
+
+if (import.meta.env.DEV) {
+	natlog({ prompts: ["log"] });
+}
 
 import { RouteDefinition, Router } from "@solidjs/router";
 import { lazy } from "solid-js";
 import { render } from "solid-js/web";
 
-//MO TODO / routes to /app, and redirects to /home if unauthorized
 const routes: RouteDefinition[] = [
+	// MO TODO route / to /app if authorized else /home
 	{ path: "/", component: lazy(() => import("./pages/Home/Home")) },
+
 	{ path: "/home", component: lazy(() => import("./pages/Home/Home")) },
+
 	{ path: "**", component: lazy(() => import("./pages/404/404")) },
 ];
 
-render(() => <Router>{routes}</Router>, document.getElementById("root")!);
+const root = document.getElementById("root");
+
+if (!root) {
+	throw new Error("missing app root");
+}
+
+render(() => <Router>{routes}</Router>, root);
