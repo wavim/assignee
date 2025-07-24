6dd525588084638baa76c0b0e43c0a68ac399079
carbonsoda <carbonsoda@outlook.com>
2025-07-24 14:57:23 +0800

    F: 404 use Link directly was dumb xD

--- frontend/src/pages/404/Info.tsx
+++ frontend/src/pages/404/Info.tsx
@@ -1,23 +1,14 @@
-import { useNavigate } from "@solidjs/router";
-import Button from "../../atoms/Button";
+import Link from "../../atoms/Link";
 import I18n from "./I18n";
 
 export default () => {
 	const t = I18n.useI18n();
 
-	const nav = useNavigate();
-
 	return (
 		<section class="bg-main flex flex-col gap-7 px-7">
 			<h1 class="font-jakarta text-text-major text-6xl font-bold">404</h1>
 			<p class="font-jakarta text-text-minor text-xl">{t("info.detail")}</p>
-			<Button
-				onclick={() => {
-					nav("/", { replace: true });
-				}}
-			>
-				{t("info.action")}
-			</Button>
+			<Link href="/">{t("info.action")}</Link>
 		</section>
 	);
 };
