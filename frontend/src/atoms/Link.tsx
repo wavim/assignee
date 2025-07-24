b899f897c0f9ef42aba0c30426831ed23733fd6a
carbonsoda <carbonsoda@outlook.com>
2025-07-24 21:26:39 +0800

    F: update Link hover anim

--- frontend/src/atoms/Link.tsx
+++ frontend/src/atoms/Link.tsx
@@ -3,8 +3,6 @@ import { A, AnchorProps } from "@solidjs/router";
 export default (props: AnchorProps) => (
 	<A
 		{...props}
-		class="bg-button font-jakarta text-text-button shadow-shadow/75 w-max rounded-full px-5 py-3 text-xl transition duration-300 ease-out select-none hover:shadow-[0.2rem_0.3rem] active:shadow-[0.2rem_0.3rem]"
-	>
-		{props.children}
-	</A>
+		class="bg-button font-jakarta text-text-button shadow-shadow/75 w-max rounded-full px-6 py-3 text-xl inset-ring-white transition-[box-shadow_filter] duration-300 ease-out hover:inset-ring-2 hover:invert"
+	></A>
 );
