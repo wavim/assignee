46756ef68686d3b9f43b3ad32b26ade22c2b034c
carbonsoda <carbonsoda@outlook.com>
2025-07-25 21:45:51 +0800

    F: 404 page rework

--- frontend/src/pages/404/404.tsx
+++ frontend/src/pages/404/404.tsx
@@ -1,16 +1,28 @@
+import Link from "../../atoms/Link";
 import Footer from "../../layouts/Footer/Footer";
 import Header from "../../layouts/Header/Header";
 import I18n from "./I18n";
-import Info from "./Info";
 
 export default () => {
 	return (
 		<I18n.I18n>
 			<Header></Header>
-			<main class="w-full flex-[1]">
+			<main class="w-full">
 				<Info></Info>
 			</main>
 			<Footer></Footer>
 		</I18n.I18n>
+	);
+};
+
+const Info = () => {
+	const t = I18n.useI18n();
+
+	return (
+		<section class="font-jakarta bg-main flex flex-col gap-8 px-8">
+			<h1 class="text-text-major text-6xl font-medium">404</h1>
+			<p class="text-text-minor text-xl">{t("detail")}</p>
+			<Link href="/">{t("link")}</Link>
+		</section>
 	);
 };
--- frontend/src/pages/404/I18n.ts
+++ frontend/src/pages/404/I18n.ts
@@ -1,11 +1,6 @@
 import { defineI18n } from "../../locales/I18n";
 
 export default defineI18n({
-	en: {
-		info: {
-			detail: "Nothing here. The page you requested is not found.",
-			action: "Go Back",
-		},
-	},
-	zh: { info: { detail: "嗯？這裡什麼也沒有。您請求的資源不存在。", action: "返回" } },
+	en: { detail: "Nothing here. The resource you requested is not found.", link: "Go Back" },
+	zh: { detail: "什麼也找不到。您請求的資源不存在。", link: "返回" },
 });
