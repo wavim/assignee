0d6c34ea51fbc17a040db180071087152aef7401
carbonsoda <carbonsoda@outlook.com>
2025-07-26 14:25:26 +0800

    F: fix button zfight due filter

--- frontend/src/atoms/Link.tsx
+++ frontend/src/atoms/Link.tsx
@@ -3,6 +3,6 @@ import { A, AnchorProps } from "@solidjs/router";
 export default (props: AnchorProps) => (
 	<A
 		{...props}
-		class="bg-button font-jakarta text-text-button shadow-shadow/75 w-max rounded-full px-6 py-3 text-xl inset-ring-white transition-[box-shadow_filter] duration-300 ease-out hover:inset-ring-2 hover:invert"
+		class="bg-button font-jakarta text-text-button shadow-shadow/75 hover:bg-text-button hover:text-button inset-ring-button w-max rounded-full px-6 py-3 text-xl inset-ring-2 transition-colors duration-300 ease-out"
 	></A>
 );
