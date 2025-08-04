3cfbec0b5ea6e62afa44c1776ea3afb98fa57c13
carbonsoda <carbonsoda@outlook.com>
2025-08-04 17:56:22 +0800

    F: button-like focus effect

--- site/client/src/gui/Button.tsx
+++ site/client/src/gui/Button.tsx
@@ -1,13 +1,12 @@
 import { twMerge } from "tailwind-merge";
 import { Props } from "../types/props";
 
-// MO TODO draft
 export default (props: Props<"button">) => (
 	<button
 		{...props}
 		type="button"
 		class={twMerge(
-			"bg-button font-jakarta text-text-button outline-button cursor-pointer rounded-xl py-3 text-xl outline-2 outline-offset-0 transition-[outline-offset] duration-100 ease-out hover:outline-offset-2",
+			"bg-button font-jakarta text-text-button outline-button cursor-pointer rounded-xl py-3 text-xl outline-2 outline-offset-0 transition-[outline-offset] duration-100 ease-out hover:outline-offset-2 focus:outline-offset-2",
 			props.class,
 		)}
 	></button>
--- site/client/src/gui/Link.tsx
+++ site/client/src/gui/Link.tsx
@@ -3,6 +3,6 @@ import { A, AnchorProps } from "@solidjs/router";
 export default (props: AnchorProps) => (
 	<A
 		{...props}
-		class="bg-button font-jakarta text-text-button outline-button w-max rounded-full px-6 py-3 text-xl outline-2 outline-offset-0 transition-[outline-offset] duration-100 ease-out hover:outline-offset-2"
+		class="bg-button font-jakarta text-text-button outline-button w-max rounded-full px-6 py-3 text-xl outline-2 outline-offset-0 transition-[outline-offset] duration-100 ease-out hover:outline-offset-2 focus:outline-offset-2"
 	></A>
 );
