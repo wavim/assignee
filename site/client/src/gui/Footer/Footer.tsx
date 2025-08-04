b740cba0467f8778cb953e13145294746f8c0959
carbonsoda <carbonsoda@outlook.com>
2025-08-04 23:49:49 +0800

    fix(F): reduce md footer mt

--- site/client/src/gui/Footer/Footer.tsx
+++ site/client/src/gui/Footer/Footer.tsx
@@ -4,7 +4,7 @@ import I18n from "./I18n";
 export default () => {
 	return (
 		<I18n.I18n>
-			<footer class="bg-main mt-12 flex w-full flex-col gap-2 p-8 md:mt-18">
+			<footer class="bg-main mt-12 flex w-full flex-col gap-2 p-8 md:mt-8">
 				<Logo class="fill-text-major h-8 md:h-4"></Logo>
 				<Copyright></Copyright>
 			</footer>
