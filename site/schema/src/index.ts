0b32246cb05965d86b0ef25dcea61d539c14f416
carbonsoda <carbonsoda@outlook.com>
2025-08-05 14:40:18 +0800

    perf(S): migrate to zod mini

--- site/schema/src/index.ts
+++ site/schema/src/index.ts
@@ -1,13 +1,13 @@
-import { z } from "zod";
+import { z } from "zod/mini";
 
 export const AuthId = z.object({
   eml: z.email(),
-  pwd: z.string().min(8).regex(/^[\x20-\x7E]*$/),
+  pwd: z.string().check(z.minLength(8), z.regex(/^[ -~]*$/)),
 });
 export type zAuthId = z.infer<typeof AuthId>;
 
 export const Bearer = z.object({
   sid: z.int(),
-  key: z.string().length(64).regex(/^[a-f\d]*$/),
+  key: z.string().check(z.length(64), z.regex(/^[a-f\d]*$/)),
 });
 export type zBearer = z.infer<typeof Bearer>;
