f5bcb1c22313cbe14caa318ee189cf7b839ee3e3
carbonsoda <carbonsoda@outlook.com>
2025-08-07 22:15:44 +0800

    style(S): add prettier

--- site/schema/rollup.config.mjs
+++ site/schema/rollup.config.mjs
@@ -3,19 +3,17 @@ import terser from "@rollup/plugin-terser";
 import typescript from "@rollup/plugin-typescript";
 import { defineConfig } from "rollup";
 
-export default defineConfig(
-  {
-    input: "src/index.ts",
-    plugins: [
-      typescript({
-        compilerOptions: {
-          declaration: true,
-          declarationDir: "./out",
-        },
-      }),
-      resolve(),
-      terser(),
-    ],
-    output: { dir: "out", format: "es" },
-  },
-);
+export default defineConfig({
+	input: "src/index.ts",
+	plugins: [
+		typescript({
+			compilerOptions: {
+				declaration: true,
+				declarationDir: "./out",
+			},
+		}),
+		resolve(),
+		terser(),
+	],
+	output: { dir: "out", format: "es" },
+});
--- site/schema/src/index.ts
+++ site/schema/src/index.ts
@@ -1,22 +1,22 @@
 // prettier-ignore
 import {
-  email, int, length as len, toLowerCase as lower, minLength as min, object as obj, positive as pos, regex, string as str, trim, infer as zType,
+	email, int, length as len, toLowerCase as lower, minLength as min, object as obj, positive as pos, regex, string as str, trim, infer as zType,
 } from "zod/mini";
 
 export const BearerToken = obj({
-  sid: int().check(pos()),
-  key: str().check(len(64), regex(/^[0-9a-f]*$/)),
+	sid: int().check(pos()),
+	key: str().check(len(64), regex(/^[0-9a-f]*$/)),
 });
 export type zBearerToken = zType<typeof BearerToken>;
 
 export const Credentials = obj({
-  mail: str().check(trim(), lower(), email()),
-  pass: str().check(min(8), regex(/^[\x20-\x7E]*$/)),
+	mail: str().check(trim(), lower(), email()),
+	pass: str().check(min(8), regex(/^[\x20-\x7E]*$/)),
 });
 export type zCredentials = zType<typeof Credentials>;
 
 export const TeamDetails = obj({
-  name: str().check(trim()),
-  desc: str().check(trim()),
+	name: str().check(trim()),
+	desc: str().check(trim()),
 });
 export type zTeamDetails = zType<typeof TeamDetails>;
