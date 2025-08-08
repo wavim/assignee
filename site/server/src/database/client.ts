b81ce39ee7fe4c5169415851e7bcd3ce6c78f98f
carbonsoda <carbonsoda@outlook.com>
2025-08-08 15:13:42 +0800

    build(B): rename native addon

--- site/server/src/database/client.ts
+++ site/server/src/database/client.ts
@@ -3,7 +3,7 @@ import { PrismaClient } from "../prisma/client";
 import { $server } from "../utils/path";
 
 if (Object.hasOwn(process, "pkg")) {
-	process.env.PRISMA_QUERY_ENGINE_LIBRARY = join($server, "out", "prisma.dll.node");
+	process.env.PRISMA_QUERY_ENGINE_LIBRARY = join($server, "out", "prisma.node");
 }
 
 export const prisma = new PrismaClient({ datasourceUrl: `file:${join(process.cwd(), "app.db")}` });
