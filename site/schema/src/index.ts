0d37cc2c625ffd728430562f658a1ee8d2d6f852
carbonsoda <carbonsoda@outlook.com>
2025-08-08 15:14:23 +0800

    feat(S): team creation payload

--- site/schema/src/index.ts
+++ site/schema/src/index.ts
@@ -3,20 +3,25 @@ import {
 	email, int, length as len, toLowerCase as lower, minLength as min, object as obj, positive as pos, regex, string as str, trim, infer as zType,
 } from "zod/mini";
 
-export const BearerToken = obj({
-	sid: int().check(pos()),
-	key: str().check(len(64), regex(/^[0-9a-f]*$/)),
-});
-export type zBearerToken = zType<typeof BearerToken>;
-
 export const Credentials = obj({
 	mail: str().check(trim(), lower(), email()),
 	pass: str().check(min(8), regex(/^[\x20-\x7E]*$/)),
 });
 export type zCredentials = zType<typeof Credentials>;
 
+export const BearerToken = obj({
+	sid: int().check(pos()),
+	key: str().check(len(64), regex(/^[0-9a-f]*$/)),
+});
+export type zBearerToken = zType<typeof BearerToken>;
+
 export const TeamDetails = obj({
 	name: str().check(trim()),
 	desc: str().check(trim()),
 });
 export type zTeamDetails = zType<typeof TeamDetails>;
+
+export const TeamCreated = obj({
+	tid: int().check(pos()),
+});
+export type zTeamCreated = zType<typeof TeamCreated>;
