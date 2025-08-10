d911ea2685f676edceb1973f6667bdfa6f39f3f1
carbonsoda <carbonsoda@outlook.com>
2025-08-10 20:08:54 +0800

    feat(F): team auth endpoints

--- site/client/src/api/team.api.ts
+++ site/client/src/api/team.api.ts
@@ -1,8 +1,24 @@
-import { TeamCreated, zTeamCreated, zTeamDetails } from "@app/schema";
+import {
+	InviterCode,
+	TeamCreated,
+	TeamPayload,
+	zInviterCode,
+	zTeamCreated,
+	zTeamDetails,
+	zTeamPayload,
+} from "@app/schema";
 import axios from "axios";
 
 const api = axios.create({ baseURL: "/api/team" });
 
 export async function create(detail: zTeamDetails): Promise<zTeamCreated> {
 	return await api.post("/create", detail).then(({ data }) => TeamCreated.parse(data));
+}
+
+export async function invite(detail: zTeamCreated): Promise<zInviterCode> {
+	return await api.post("/invite", detail).then(({ data }) => InviterCode.parse(data));
+}
+
+export async function access(detail: zInviterCode): Promise<zTeamPayload> {
+	return await api.post("/access", detail).then(({ data }) => TeamPayload.parse(data));
 }
