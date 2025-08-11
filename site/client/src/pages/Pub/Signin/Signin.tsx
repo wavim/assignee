b74b782759eb5082beb98a8f3690d549e8eee717
carbonsoda <carbonsoda@outlook.com>
2025-08-11 13:44:06 +0800

    feat(F): signer error default to 500

--- site/client/src/pages/Pub/Signin/Signin.tsx
+++ site/client/src/pages/Pub/Signin/Signin.tsx
@@ -37,7 +37,7 @@ const Form = () => {
 			case ErrorCode.TOO_MANY_REQUESTS: {
 				return setError("errors.ratelim");
 			}
-			case ErrorCode.INTERNAL_SERVER_ERROR: {
+			default: {
 				return setError("errors.systems");
 			}
 		}
