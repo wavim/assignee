64887255771c9116030e2b0ef63e5b4523ddeb4e
carbonsoda <carbonsoda@outlook.com>
2025-08-18 02:29:54 +0800

    feat(S): task system schemas

--- site/schema/src/index.ts
+++ site/schema/src/index.ts
@@ -145,3 +145,66 @@ export const GetTeamTasksResults = z.discriminatedUnion("auth", [
 	}),
 ]);
 export type GetTeamTasksResults = z.infer<typeof GetTeamTasksResults>;
+
+// GET /tasks/:aid
+
+export const GetTaskRequest = z.object({
+	aid: z.string().check(z.regex(/^[0-9a-zA-Z]{8,}$/)),
+});
+export type GetTaskRequest = z.infer<typeof GetTaskRequest>;
+
+export const GetTaskResults = z.discriminatedUnion("auth", [
+	z.object({
+		tid: z.string().check(z.regex(/^[0-9a-zA-Z]{8,}$/)),
+		auth: z.literal(true),
+		name: z.string().check(z.trim(), z.minLength(1)),
+		desc: z.string().check(z.trim(), z.minLength(1)),
+		dead: z.iso.datetime(),
+		file: z.optional(z.string().check(z.trim(), z.minLength(1))),
+		works: z.array(
+			z.object({
+				name: z.string().check(z.trim(), z.minLength(1)),
+				mail: z.string().check(z.trim(), z.email(), z.toLowerCase()),
+				file: z.optional(z.string().check(z.trim(), z.minLength(1))),
+				done: z.boolean(),
+				comm: z.optional(z.string().check(z.trim())),
+			}),
+		),
+	}),
+	z.object({
+		tid: z.string().check(z.regex(/^[0-9a-zA-Z]{8,}$/)),
+		auth: z.literal(false),
+		name: z.string().check(z.trim(), z.minLength(1)),
+		desc: z.string().check(z.trim(), z.minLength(1)),
+		dead: z.iso.datetime(),
+		file: z.optional(z.string().check(z.trim(), z.minLength(1))),
+		work: z.object({
+			file: z.optional(z.string().check(z.trim(), z.minLength(1))),
+			done: z.boolean(),
+			comm: z.optional(z.string().check(z.trim())),
+		}),
+	}),
+]);
+export type GetTaskResults = z.infer<typeof GetTaskResults>;
+
+// PUT /works/:aid
+
+export const PutWorkRequest = z.object({
+	done: z.boolean(),
+});
+export type PutWorkRequest = z.infer<typeof PutWorkRequest>;
+
+// PUT /works/:aid/comm
+
+export const PutUserTaskCommRequest = z.object({
+	mail: z.string().check(z.trim(), z.email(), z.toLowerCase()),
+	comm: z.optional(z.string().check(z.trim())),
+});
+export type PutUserTaskCommRequest = z.infer<typeof PutUserTaskCommRequest>;
+
+// GET /works/:aid/:mail/file
+
+export const GetUserTaskWorkRequest = z.object({
+	mail: z.string().check(z.trim(), z.email(), z.toLowerCase()),
+});
+export type GetUserTaskWorkRequest = z.infer<typeof GetUserTaskWorkRequest>;
