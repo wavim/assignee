fa016cc214aeaf8b3acf358367d321aec7e70a8c
carbonsoda <carbonsoda@outlook.com>
2025-08-18 02:32:04 +0800

    feat(B): apply ccontrol headers

--- site/server/src/routes/code.route.ts
+++ site/server/src/routes/code.route.ts
@@ -2,10 +2,13 @@ import { PutMembRequest } from "@app/schema";
 import { ErrorCode, HttpError } from "@wavim/http-error";
 import { Router } from "express";
 import { authen } from "../middleware/authen";
+import { ccontrol } from "../middleware/ccache";
 import { member } from "../middleware/member";
 import { getCode, putMemb } from "../services/code.service";
 
 export const code = Router()
+	.use(ccontrol("no-cache"))
+
 	.get("/teams/:tid/code", authen, member, async (req, res) => {
 		try {
 			if (!req.own) {
--- site/server/src/routes/team.route.ts
+++ site/server/src/routes/team.route.ts
@@ -1,11 +1,14 @@
 import { PostTeamRequest } from "@app/schema";
-import { ErrorCode, HttpError } from "@wavim/http-error";
+import { ErrorCode } from "@wavim/http-error";
 import { Router } from "express";
 import { authen } from "../middleware/authen";
+import { ccontrol } from "../middleware/ccache";
 import { member } from "../middleware/member";
 import { createTeam, queryTeams, teamDetail } from "../services/team.service";
 
 export const team = Router()
+	.use(ccontrol("no-cache"))
+
 	.post("/teams", authen, async (req, res) => {
 		const { success, error, data } = PostTeamRequest.safeParse(req.body);
 
@@ -28,10 +31,7 @@ export const team = Router()
 	.get("/teams/:tid", authen, member, async (req, res) => {
 		try {
 			res.json(await teamDetail(req.tid, req.own));
-		} catch (e) {
-			if (e instanceof HttpError) {
-				return res.status(e.status).send(e.message);
-			}
+		} catch {
 			res.sendStatus(ErrorCode.INTERNAL_SERVER_ERROR);
 		}
 	});
