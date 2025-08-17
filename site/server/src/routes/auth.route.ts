5429ecba8c48ce1d0cc1c7c8b88fc4bb03918461
carbonsoda <carbonsoda@outlook.com>
2025-08-18 02:31:23 +0800

    fix(B): remove end()s

--- site/server/src/routes/auth.route.ts
+++ site/server/src/routes/auth.route.ts
@@ -4,20 +4,28 @@ import { Router } from "express";
 import rateLimit from "express-rate-limit";
 import { configs } from "../configs/configs";
 import { authen } from "../middleware/authen";
+import { ccontrol } from "../middleware/ccache";
 import { logout, rotate, signin, signup } from "../services/auth.service";
 import { addtime } from "../utils/time";
 
 const tok = (tok: SessionCookie) => {
-	return ["tok", tok, { httpOnly: true, expires: addtime(configs.sessAge) }] as const;
+	return [
+		"tok",
+		tok,
+		// MO NOTE serving over HTTP for DEMO ONLY
+		{ httpOnly: true, secure: false, expires: addtime(configs.sessAge) },
+	] as const;
 };
 
 export const auth = Router()
+	.use(ccontrol("no-cache"))
+
 	.post("/auth/verify", authen, async (req, res) => {
 		if (!req.rot) {
-			return res.end();
+			return res.send(0);
 		}
 		try {
-			res.cookie(...tok(await rotate(req.sid))).end();
+			res.cookie(...tok(await rotate(req.sid))).send(0);
 		} catch {
 			res.sendStatus(ErrorCode.INTERNAL_SERVER_ERROR);
 		}
@@ -25,11 +33,12 @@ export const auth = Router()
 	.post("/auth/logout", authen, async (req, res) => {
 		try {
 			await logout(req.sid);
-			res.clearCookie("tok", { httpOnly: true }).end();
+			res.clearCookie("tok", { httpOnly: true, secure: true }).send(0);
 		} catch {
 			res.sendStatus(ErrorCode.INTERNAL_SERVER_ERROR);
 		}
 	})
+
 	.post("/auth/signin", rateLimit(configs.rateLim["auth/signin"]), async (req, res) => {
 		const { success, error, data } = SigninRequest.safeParse(req.body);
 
@@ -37,7 +46,7 @@ export const auth = Router()
 			return res.status(ErrorCode.BAD_REQUEST).send(error);
 		}
 		try {
-			res.cookie(...tok(await signin(data))).end();
+			res.cookie(...tok(await signin(data))).send(0);
 		} catch (e) {
 			if (e instanceof HttpError) {
 				return res.status(e.status).send(e.message);
@@ -52,7 +61,7 @@ export const auth = Router()
 			return res.status(ErrorCode.BAD_REQUEST).send(error);
 		}
 		try {
-			res.cookie(...tok(await signup(data))).end();
+			res.cookie(...tok(await signup(data))).send(0);
 		} catch (e) {
 			if (e instanceof HttpError) {
 				return res.status(e.status).send(e.message);
